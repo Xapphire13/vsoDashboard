8fa2ca2cbee7d072e9c30202cce36b7e89a46b01
Steven Hobson-Campbell <stevhob@microsoft.com>
2016-12-02 00:22:36 -0800

    Fixing static paths for Azure, and adding web.config file

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,7 @@
 
 import * as express from "express";
 import {ServerOAuthHelper} from "./ServerOAuthHelper";
-eval(require('fs').readFileSync('./shared/StringFormat.js', 'utf8'));
+eval(require('fs').readFileSync('../shared/StringFormat.js', 'utf8'));
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 let clientSecret = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiIxNDM5ZmYyZC0yNmFiLTQwNDktYTU0My01YWZmNDg0OGVlYzgiLCJjc2kiOiI3Y2RhYmY0ZC1jOGM5LTRkYTYtOWNlMC1iOWIzZmM1Mzg0N2EiLCJuYW1laWQiOiIyNzkyYWI2MS1iMDQwLTRmODItYjM5NC0wZGY4ZjMwYzkxMDUiLCJpc3MiOiJhcHAudnNzcHMudmlzdWFsc3R1ZGlvLmNvbSIsImF1ZCI6ImFwcC52c3Nwcy52aXN1YWxzdHVkaW8uY29tIiwibmJmIjoxNDgwMzgxNDI4LCJleHAiOjE2MzgxNDc4Mjh9.GAMTR0tzBMWvXC2BNrp0RbPle7dX8QbSziu601xRMzXfg666kqN7fF5H5v5XZoKvlg7jKVPJFEQXhNvMLoElXQQn0FnzVGzt5pu1pmVsd9EFW-ao6W6r8rln0GEN83BNZLlNGWqK8lpB2WB-ikkGOsvJpJM2MQu22I9ZYKR2C0mjfs7dLvw7RdHM72RH-8HHYPBFkW17Yxa2lIjBLJrQBEnVe2SsIKv9w3duaOrnYZxgXZ_F2pb-NZrNYC7N3t3uuZ03Tzwx0qNI3n7zSRFxUv5VE2E9HWnzSK3wKLlMltv-GPrXYn9Dh6paqgHNDeJO8NDLVxSTW2vCXwSnmwDxNw";
@@ -12,9 +12,9 @@ let app = express();
 app.set('port', process.env.PORT || 80);
 
 // Static files
-app.use(express.static("client"));
-app.use("/libs", express.static("bower_components"));
-app.use("/scripts", express.static("shared"));
+app.use(express.static("../client"));
+app.use("/libs", express.static("../bower_components"));
+app.use("/scripts", express.static("../shared"));
 
 // OAuth Routing
 app.get("/auth", (req, res) => {
