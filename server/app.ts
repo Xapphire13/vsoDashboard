a074a4c99445ad93626ade2bbd0e5c216959a6e0
Steven Hobson-Campbell <stevhob@microsoft.com>
2016-12-01 17:21:42 -0800

    Chaning port info

--- server/app.ts
+++ server/app.ts
@@ -5,10 +5,11 @@ import {ServerOAuthHelper} from "./ServerOAuthHelper";
 eval(require('fs').readFileSync('./shared/StringFormat.js', 'utf8'));
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-let port = 80;
 let clientSecret = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiIxNDM5ZmYyZC0yNmFiLTQwNDktYTU0My01YWZmNDg0OGVlYzgiLCJjc2kiOiI3Y2RhYmY0ZC1jOGM5LTRkYTYtOWNlMC1iOWIzZmM1Mzg0N2EiLCJuYW1laWQiOiIyNzkyYWI2MS1iMDQwLTRmODItYjM5NC0wZGY4ZjMwYzkxMDUiLCJpc3MiOiJhcHAudnNzcHMudmlzdWFsc3R1ZGlvLmNvbSIsImF1ZCI6ImFwcC52c3Nwcy52aXN1YWxzdHVkaW8uY29tIiwibmJmIjoxNDgwMzgxNDI4LCJleHAiOjE2MzgxNDc4Mjh9.GAMTR0tzBMWvXC2BNrp0RbPle7dX8QbSziu601xRMzXfg666kqN7fF5H5v5XZoKvlg7jKVPJFEQXhNvMLoElXQQn0FnzVGzt5pu1pmVsd9EFW-ao6W6r8rln0GEN83BNZLlNGWqK8lpB2WB-ikkGOsvJpJM2MQu22I9ZYKR2C0mjfs7dLvw7RdHM72RH-8HHYPBFkW17Yxa2lIjBLJrQBEnVe2SsIKv9w3duaOrnYZxgXZ_F2pb-NZrNYC7N3t3uuZ03Tzwx0qNI3n7zSRFxUv5VE2E9HWnzSK3wKLlMltv-GPrXYn9Dh6paqgHNDeJO8NDLVxSTW2vCXwSnmwDxNw";
 let redirectUri = "http://vsodash.azurewebsites.net/auth";
 let app = express();
+
+app.set('port', process.env.PORT || 80);
 
 // Static files
 app.use(express.static("client"));
@@ -45,6 +46,6 @@ app.get("/token", (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
-  console.log("Listening on port " + port);
+let server = app.listen(app.get('port'), () => {
+  console.log("Listening on port " + server.address().port);
 });
