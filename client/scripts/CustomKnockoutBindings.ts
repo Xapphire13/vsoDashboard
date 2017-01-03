8152fb32f300232e6f9a0f10f5e92fa0c20875cc
Steven Hobson-Campbell <stevhob@microsoft.com>
2017-01-03 11:26:42 -0800

    Simplifying KO bindings

--- client/scripts/CustomKnockoutBindings.ts
+++ client/scripts/CustomKnockoutBindings.ts
@@ -5,60 +5,26 @@ import {ControlBase} from "./controls/ControlBase";
 import {IViewModel} from "./models/IViewModel";
 
 let handlers: {[key: string]: KnockoutBindingHandler} = {
-  boundHtml: {
+  viewModel: {
     init: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
       return { controlsDescendantBindings: true };
     },
     update: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
-      let htmlLoader: Q.Promise<string> = ko.unwrap(valueAccessor());
-
-      if(htmlLoader != undefined) {
-        let $element = $(element);
-        $element.empty();
-
-        htmlLoader.then(html => {
-          $element.append(html);
-          ko.applyBindingsToDescendants(bindingContext, $element[0]);
-        }, reason => {
-          console.error(reason);
-        });
-      }
-    }
-  },
-  control: {
-    init: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
-      return { controlsDescendantBindings: true };
-    },
-    update: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
-      let $element = $(element);
-      let controlViewModel: ControlBase = ko.unwrap(valueAccessor());
-
-      if(controlViewModel != null) {
-        let childBindingContext: KnockoutBindingContext = bindingContext.createChildContext(controlViewModel);
-        controlViewModel.getHtml().then(html => {
-          let $html = $(html);
-          $element.empty();
-          $element.append($html);
-          ko.applyBindingsToDescendants(childBindingContext, $element[0]);
-        }).fail(reason => {
-          console.error(reason);
-        });
-      }
-    }
-  },
-  view: {
-    init: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
-      return { controlsDescendantBindings: true };
-    },
-    update: (element: HTMLElement, valueAccessor, allBindings, viewModel, bindingContext) => {
-      let view: IViewModel = ko.unwrap(valueAccessor());
+      let view: IViewModel | ControlBase = ko.unwrap(valueAccessor());
 
       if(view != undefined) {
         let $element = $(element);
         $element.empty();
         let childBindingContext: KnockoutBindingContext = bindingContext.createChildContext(view);
 
-        ContentLoader.loadView(view.viewName).then(html => {
+        let fetchHtml: Q.Promise<string>;
+        if((<ControlBase>view).getHtml != undefined) {
+          fetchHtml = (<ControlBase>view).getHtml();
+        } else if ((<IViewModel>view).viewName != undefined) {
+          fetchHtml = ContentLoader.loadView((<IViewModel>view).viewName);
+        }
+
+        fetchHtml.then(html => {
           $element.append(html);
           ko.applyBindingsToDescendants(childBindingContext, $element[0]);
         }, reason => {
