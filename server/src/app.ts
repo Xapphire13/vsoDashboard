ea9afd4a7640f1e784c070f9835f23cef5c7a02d
REDMOND\jercu <jjcummins@outlook.com>
2017-07-24 13:13:36 -0700

    A few more changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import * as path from "path";
 import * as logger from "morgan";
 import * as cookieParser from "cookie-parser";
-var bodyParser = require('body-parser');
+import * as bodyParser from "body-parser";
 
 var index = require('./routes/index');
 var users = require('./routes/users');
