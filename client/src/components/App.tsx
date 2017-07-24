a06e32b570c73ed62588aa5ea6387ae8649fe76c
Melting Mosaic <meltingmosaic@noreply.github.com>
2017-07-24 16:40:05 -0700

    Made main menu work

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,35 @@
 import * as React from "react";
 
 import {Header} from "./Header";
-import {RepoList} from "./RepoList";
+import {PullRequestArea} from "./PullRequestArea";
+import {SettingsArea} from "./SettingsArea";
+import {WorkItemsArea} from "./WorkItemsArea";
 
-export class App extends React.Component {
+export class App extends React.Component<{}, {selectedArea: string}> {
+    public constructor() {
+        super();
+        this.state = {selectedArea: "pullRequests"};
+    }
+
     public render(): JSX.Element {
+        let area: JSX.Element;
+        if (this.state.selectedArea === "pullRequests") {
+            area = <PullRequestArea />;
+        } else if (this.state.selectedArea === "workItems") {
+            area = <WorkItemsArea />;
+        } else if (this.state.selectedArea === "settings") {
+            area = <SettingsArea />;
+        } else {
+            area = <div />;
+        }
+
         return <div>
-          <Header />
-          <RepoList/>
+          <Header onSelectedChanged={this._onMenuSelectionChanged}/>
+          { area }
         </div>;
+    }
+
+    private _onMenuSelectionChanged = (s: string):void => {
+        this.setState({selectedArea: s});
     }
 }
---
+++ client/src/components/WorkItemsArea.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class WorkItemsArea extends React.Component {
+    public render(): JSX.Element {
+        return <p>Work items area</p>;
+    }
+}
