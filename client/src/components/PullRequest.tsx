f8ba4bb0dbbb9c1253302b6c43d499912061711e
Steven Hobson-Campbell <xapphire13@live.com>
2017-07-25 13:44:44 -0700

    Adding icons

--- client/src/components/PullRequest.tsx
+++ client/src/components/PullRequest.tsx
@@ -2,6 +2,8 @@ import "../styles/pullRequest.less";
 
 import * as React from "react";
 
+import {getIcon, Icon} from "../icons";
+
 declare type Propeties = {
   title: string,
   myStatus: string,
@@ -15,13 +17,23 @@ declare type Propeties = {
 export class PullRequest extends React.Component<Propeties, {}> {
   public render(): JSX.Element {
     return <tr className="pullRequest">
-      <td>{this.props.numberOfComments}</td>
+      <td>
+        <div className="comments">
+          {getIcon(Icon.message)}
+          {this.props.numberOfComments}
+        </div>
+      </td>
       <td>{this.props.title}</td>
       <td>{this.props.myStatus}</td>
       <td>{this.props.status}</td>
       <td>{this.props.createBy}</td>
       <td>{this.props.created}</td>
       <td>{this.props.updated}</td>
+      <td>
+        <div className="quickActions clickable">
+          {getIcon(Icon.mail)}
+        </div>
+      </td>
     </tr>;
   }
 }
