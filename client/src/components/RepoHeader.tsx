021364c1322b733b43d7e403874aa2b716faefea
Steven Hobson-Campbell <xapphire13@live.com>
2017-07-24 17:45:52 -0700

    Styling the PR table

--- client/src/components/RepoHeader.tsx
+++ client/src/components/RepoHeader.tsx
@@ -1,8 +1,12 @@
+import "../styles/repoHeader.less";
+
 import * as React from "react";
 
-export class RepoHeader extends React.Component {
+export class RepoHeader extends React.Component<{name: string}, {}> {
   public render(): JSX.Element {
-
-    return <h2>Repo header</h2>;
+    return <div className="repoHeader">
+      <span>&#x25E2;</span>
+      <h4>{this.props.name}</h4>
+     </div>;
   }
 }
--- client/src/components/RepoList.tsx
+++ client/src/components/RepoList.tsx
@@ -5,8 +5,8 @@ import {Repo} from './Repo';
 export class RepoList extends React.Component {
   public render(): JSX.Element {
     const repos: JSX.Element[] = [];
-    repos.push(<Repo/>);
-    repos.push(<Repo/>);
+    repos.push(<Repo name="Test Repo"/>);
+    repos.push(<Repo name="Super cool repo"/>);
 
     return <div>
       {repos}
