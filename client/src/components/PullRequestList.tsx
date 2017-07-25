681c73a95d33fc988edda4c62509276ff6848647
Steven <xapphire13@live.com>
2017-07-24 17:47:39 -0700

    Styling the PR table (#12)

--- client/src/components/PullRequestList.tsx
+++ client/src/components/PullRequestList.tsx
@@ -1,7 +1,57 @@
+import "../styles/pullRequestList.less";
+
 import * as React from "react";
+
+import {PullRequest} from "./PullRequest";
 
 export class PullRequestList extends React.Component {
   public render(): JSX.Element {
-    return <h2>PR List</h2>;
+    const pullRequests = [
+      {
+        title: "Test PR",
+        myStatus: "Assigned",
+        status: "Active",
+        createBy: "Joe Bloggs",
+        created: "2017-04-23T18:00:00.000Z",
+        updated: "2012-04-24T18:00:00.000Z",
+        numberOfComments: 10
+      },
+      {
+        title: "Test PR 2",
+        myStatus: "Assigned",
+        status: "Active",
+        createBy: "Joe Bloggs",
+        created: "2017-04-23T18:00:00.000Z",
+        updated: "2012-04-24T18:00:00.000Z",
+        numberOfComments: 12
+      },
+      {
+        title: "Test PR 3",
+        myStatus: "Assigned",
+        status: "Active",
+        createBy: "Joe Bloggs",
+        created: "2017-04-23T18:00:00.000Z",
+        updated: "2012-04-24T18:00:00.000Z",
+        numberOfComments: 0
+      },
+    ];
+
+    return <div className="pullRequestList">
+      <div className="columnHeader first">Title</div>
+      <div className="columnHeader">My Status</div>
+      <div className="columnHeader">Status</div>
+      <div className="columnHeader">Created by</div>
+      <div className="columnHeader">Created</div>
+      <div className="columnHeader">Updated</div>
+      {pullRequests.map(pullRequest => <PullRequest
+          title={pullRequest.title}
+          myStatus={pullRequest.myStatus}
+          status={pullRequest.status}
+          createBy={pullRequest.status}
+          created={pullRequest.created}
+          updated={pullRequest.updated}
+          numberOfComments={pullRequest.numberOfComments}
+        />)}
+    </div>;
   }
 }
--- client/src/components/Repo.tsx
+++ client/src/components/Repo.tsx
@@ -1,15 +1,19 @@
+import "../styles/repo.less";
+
 import * as React from "react";
 
 import {PullRequestList} from "./PullRequestList";
 import {RepoChartContainer} from "./RepoChartContainer";
 import {RepoHeader} from "./RepoHeader";
 
-export class Repo extends React.Component {
+export class Repo extends React.Component<{name: string}, {}> {
   public render(): JSX.Element {
-    return <div>
-      <RepoHeader />
-      <PullRequestList />
-      <RepoChartContainer />
+    return <div className="repo">
+      <RepoHeader name={this.props.name}/>
+      <div className="repoContent">
+        <PullRequestList />
+        <RepoChartContainer />
+      </div>
     </div>;
   }
 }
