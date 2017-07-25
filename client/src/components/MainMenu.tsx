9b2c88ef238e14e58632337912927bcac1937679
Melting Mosaic <meltingmosaic@noreply.github.com>
2017-07-25 15:34:11 -0700

    Add selection caret

--- client/src/components/MainMenu.tsx
+++ client/src/components/MainMenu.tsx
@@ -19,10 +19,25 @@ export class MainMenu extends React.Component<{ onSelectedChanged(item: string):
 
   public render(): JSX.Element {
     return <ul className="mainMenu">
+      <svg height="0" width="0">
+        <defs>
+            <linearGradient id="gradient">
+              <stop offset="0%" stopColor="transparent" />
+              <stop offset="50%" stopColor="white" stopOpacity="75"/>
+              <stop offset="100%" stopColor="transparent" />
+            </linearGradient>
+          </defs>
+      </svg>
       {Object.keys(this._menuItems).map((item, index) => <li key={index}>
         <a className={item === this.state.selectedItem ? "selected" : ""} onClick={(e) => { this._handleClick(e); this._processMenuChange(item); }}>
           {this._menuItems[item]}
         </a>
+        <svg className={`caret ${item === this.state.selectedItem ? "selected" : ""}`}>
+          <polygon points="45 7 50 0 55 7" fill="#242424" strokeWidth="0" />
+          <polyline points="0 6, 45 6, 50 0, 55 6, 100 6"
+            stroke="url(#gradient)" strokeWidth="0.75" strokeLinejoin="miter" strokeLinecap="butt"
+            fill="transparent"/>
+        </svg>
       </li>)}
     </ul>;
   }
