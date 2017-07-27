72d6abc3b6ae70229b4e338ca4119abd6627cf5b
MeltingMosaic <MeltingMosaic@users.noreply.github.com>
2017-07-27 11:35:19 -0700

    Repolist (#33)
    * Basic list of repos
    
    * basic selection
    
    * Add/remove repo working, unstyled
    
    * More repo selection
    
    * Repo add/remove working

--- client/src/components/SettingsArea.tsx
+++ client/src/components/SettingsArea.tsx
@@ -4,9 +4,11 @@ import * as Preferences from "../api/Preferences";
 import * as React from "react";
 import * as VsoApi from "../api/VsoApi";
 
-import {FilteredMultiSelect} from "./FilteredMultiSelect";
-import {IPreferences} from "../../../server/src/IPreferences";
-import {IRepository} from "../api/models/IRepository";
+import { FilteredMultiSelect } from "./FilteredMultiSelect";
+import { IPreferences } from "../../../server/src/IPreferences";
+import { IRepository } from "../api/models/IRepository";
+import { IRepositoryPreference } from "../../../server/src/IRepositoryPreference";
+import { SortColumns } from "../../../server/src/SortColumns";
 
 declare type Props = {
   preferences: IPreferences | null
@@ -14,7 +16,8 @@ declare type Props = {
 
 declare type State = {
   preferences: IPreferences | null
-  availableRepos: IRepository[]
+  availableRepos: IRepository[],
+  selectedRepos: IRepository[]
 };
 
 export class SettingsArea extends React.Component<Props, State> {
@@ -23,14 +26,29 @@ export class SettingsArea extends React.Component<Props, State> {
 
     this.state = {
       preferences: props.preferences,
-      availableRepos: []
+      availableRepos: [],
+      selectedRepos: []
     };
   }
 
   public async componentDidMount(): Promise<void> {
     const repos: IRepository[] = await VsoApi.listRepositories();
+    const selectedRepos: IRepository[] = [];
+    if (this.state.preferences && this.state.preferences.repositoryPreferences.length > 0) {
+      this.state.preferences.repositoryPreferences.forEach(p => {
+        const foundRepo: IRepository | undefined = repos.find(r => r.id === p.repositoryId);
+        if (foundRepo) {
+          selectedRepos.push(foundRepo);
+        }
+      });
+    }
 
-    this.setState({availableRepos: repos});
+    this.setState({
+      availableRepos: repos.sort((a: IRepository, b: IRepository) => {
+        return a.name > b.name ? 1 : -1;
+      }),
+      selectedRepos: selectedRepos
+    });
   }
 
   public componentWillReceiveProps(nextProps: Props): void {
@@ -47,7 +65,7 @@ export class SettingsArea extends React.Component<Props, State> {
         <input
           type="number"
           min="1" value={this.state.preferences ? this.state.preferences.pollIntervalInSeconds : NaN}
-          onChange={(e) => this._handleInput("pollIntervalInSeconds", e)}/>
+          onChange={(e) => this._handleInput("pollIntervalInSeconds", e)} />
       </label>
       <label className="setting">
         Stale threshold (minutes)
@@ -55,25 +73,69 @@ export class SettingsArea extends React.Component<Props, State> {
           type="number"
           min="1"
           value={this.state.preferences ? this.state.preferences.staleThresholdInMinutes : NaN}
-          onChange={(e) => this._handleInput("staleThresholdInMinutes", e)}/>
+          onChange={(e) => this._handleInput("staleThresholdInMinutes", e)} />
       </label>
-      <button className="primary" onClick={() => this.state.preferences && Preferences.savePreferences(this.state.preferences).then(() => alert("Saved!"))}>Save</button>
-      <h3>Repositories</h3>
+      <h2>Repositories</h2>
       <div>
-        <FilteredMultiSelect defaultFilter="" textProp="" options={this.state.availableRepos.map(r => r.name)} onChange={() => null}/>
-        {this.state.preferences && this.state.preferences.repositoryPreferences && this.state.preferences.repositoryPreferences.map(repo => <div key={repo.repositoryId}>{JSON.stringify(repo)}</div>)}
+        <FilteredMultiSelect
+          defaultFilter=""
+          textProp="name"
+          valueProp="name"
+          size="15"
+          options={this.state.availableRepos}
+          selectedOptions={this.state.selectedRepos}
+          onChange={this._handleSelectionChange} />
+        {/* {this.state.preferences && this.state.preferences.repositoryPreferences && this.state.preferences.repositoryPreferences.map(repo => <div key={repo.repositoryId}>{JSON.stringify(repo)}</div>)} */}
       </div>
+      <div>
+        <h3>Selected Repositories</h3>
+        {this.state.selectedRepos.length === 0 && "No repositories selected"}
+        {this.state.selectedRepos.length > 0 &&
+          this.state.selectedRepos.map((r, i) => <ul>{r.name}<span style={{ cursor: "pointer" }} onClick={() => this._handleDeselect(i)}>
+            &times;
+            </span></ul>)}
+      </div>
+      <button className="primary" onClick={this._onSave}>Save</button>
     </div>;
   }
 
+  private _onSave = (): void => {
+    if (this.state.preferences) {
+      const preferences: IPreferences = this.state.preferences;
+      preferences.repositoryPreferences = this.state.selectedRepos.map(this._convertToRepositoryPreference);
+      Preferences.savePreferences(preferences).then(() => alert("Saved!"));
+      this.setState({ preferences: preferences });
+    }
+  }
+
   private _handleInput = (key: string, event: React.ChangeEvent<HTMLInputElement>) => {
-    if(this.state.preferences) {
-      const preferences: IPreferences & {[key: string]: any} = this.state.preferences;
+    if (this.state.preferences) {
+      const preferences: IPreferences & { [key: string]: any } = this.state.preferences;
       preferences[key] = +event.target.value;
 
       this.setState({
         preferences
       });
     }
+  }
+
+  private _handleDeselect(index: number): void {
+    let selectedRepos: IRepository[] = this.state.selectedRepos.slice();
+    selectedRepos.splice(index, 1);
+    this.setState({ selectedRepos: selectedRepos });
+  }
+
+  private _handleSelectionChange = (selections: IRepository[]) => {
+    selections.sort((a, b) => a.name > b.name ? 1 : -1);
+    this.setState({ selectedRepos: selections });
+  }
+
+  private _convertToRepositoryPreference(repo: IRepository): IRepositoryPreference {
+    return {
+      repositoryId: repo.id,
+      justMine: true,
+      isMinimised: false,
+      sortPreferences: [{ column: SortColumns.title, isAssending: true, presidence: 0 }]
+    };
   }
 }
